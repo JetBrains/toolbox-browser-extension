de19ff783d972c14efc6b608016b8cc7457eb2c8
Victor Kropp <victor.kropp@jetbrains.com>
2019-04-24 15:37:32 +0200

    Fix CI build

--- prepare-metadata.sh
+++ prepare-metadata.sh
@@ -2,7 +2,7 @@
 
 set -e -x -u
 
-printenv CHROME_EXTENSION_PEM_BASE64 | base64 -d > extension.pem
+printenv CHROME_EXTENSION_PEM_BASE64 | tr -d '\r' | base64 -d > extension.pem
 
 sed -i "s/\"version\": \"1.0\"/\"version\": \"$BUILD_NUMBER\"/g" manifest.json
 
