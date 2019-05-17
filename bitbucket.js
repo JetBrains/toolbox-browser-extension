15575b963e48d898514b4b4e51eb64f152bfec9d
Daniil Penkin <dpenkin@atlassian.com>
2019-05-17 11:56:49 +1000

    PR review - Refactor and improve
    - Debounce handling UI change
    - Refactor CSS stylesheet injection
    - Reformat according to eslint rules

--- bitbucket.js
+++ bitbucket.js
@@ -1,3 +1,4 @@
+import {debounce} from 'throttle-debounce';
 import 'whatwg-fetch';
 
 import {
@@ -6,53 +7,61 @@ import {
   supportedTools
 } from './common';
 
-let styleSheetAdded = false;
-let addingButtons = false;
+const MUTATION_DEBOUNCE_DELAY = 300;
 
 function selectTools(language) {
   // All languages in Bitbucket match the common list with an exception of HTML
-  if (language === 'html/css') {
-    language = 'html';
-  }
+  const lang = language === 'html/css' ? 'html' : language;
 
-  const selectedToolIds = language && supportedLanguages[language.toLowerCase()];
-  return selectedToolIds.length ? selectedToolIds : ['idea'];
+  const selectedToolIds = lang && supportedLanguages[lang.toLowerCase()];
+  return selectedToolIds && selectedToolIds.length ? selectedToolIds : ['idea'];
 }
 
-function configureStyleSheet(fileName) {
-  if (!styleSheetAdded) {
-    const styleSheet = document.createElement("style");
-    // language=CSS
-    styleSheet.innerHTML = `
-      .jt-button {
-        margin: 0 2px;
-      }
-
-      .jt-button:hover {
-        background: rgba(9, 30, 66, 0.08);
-        cursor: pointer;
-      }
-
-      .jt-button img {
-        align-self: center;
-        width: 20px;
-        height: 20px;
-      }
-    `;
-
-    document.head.appendChild(styleSheet);
-    styleSheetAdded = true;
+function configureStyleSheet() {
+  const styleId = 'jt-bitbucket-style';
+  if (document.getElementById(styleId)) {
+    return;
   }
+
+  const styleSheet = document.createElement('style');
+  styleSheet.id = styleId;
+  // language=CSS
+  styleSheet.innerHTML = `
+    .jt-button {
+      margin: 0 2px;
+    }
+
+    .jt-button:hover {
+      background: rgba(9, 30, 66, 0.08);
+      cursor: pointer;
+    }
+
+    .jt-button img {
+      align-self: center;
+      width: 20px;
+      height: 20px;
+    }
+  `;
+
+  document.head.appendChild(styleSheet);
+}
+
+function buttonsShown() {
+  return !!document.getElementsByClassName('jt-button').length;
 }
 
 function renderButtons(tools, cloneCheckoutButton, cloneUrl) {
+  if (buttonsShown()) {
+    return;
+  }
+
   configureStyleSheet();
 
   const buttonGroup = document.createElement('div');
 
-  tools
-    .map(toolId => supportedTools[toolId])
-    .forEach(tool => {
+  tools.
+    map(toolId => supportedTools[toolId]).
+    forEach(tool => {
       const btn = document.createElement('a');
       btn.setAttribute('class', `${cloneCheckoutButton.className} jt-button`);
       btn.setAttribute('href', getToolboxURN(tool.tag, cloneUrl));
@@ -75,9 +84,8 @@ function getSshCloneUrl(links) {
   return null;
 }
 
-function handleUiChange() {
-  if (addingButtons || document.getElementsByClassName('jt-button').length) {
-    // Buttons are being added or already exist in the DOM
+const handleUiChange = debounce(MUTATION_DEBOUNCE_DELAY, () => {
+  if (buttonsShown()) {
     return;
   }
 
@@ -85,29 +93,26 @@ function handleUiChange() {
   const cloneCheckoutButton = document.querySelector('[data-qa="page-header-wrapper"] [class*="ActionsWrapper"] [type="button"]:not([aria-expanded])');
 
   if (fullSlug && cloneCheckoutButton) {
-    addingButtons = true;
     const metadataUrl = `${window.location.origin}/!api/2.0/repositories/${fullSlug[1]}?fields=language,links.clone`;
 
-    fetch(metadataUrl)
-      .then(response => response.json())
-      .then(rs => {
+    fetch(metadataUrl).
+      then(response => response.json()).
+      then(rs => {
         const tools = selectTools(rs.language);
         const cloneUrl = getSshCloneUrl(rs.links);
         renderButtons(tools, cloneCheckoutButton, cloneUrl);
-      })
-      .catch(() => { /*Do nothing.*/ })
-      .finally(() => addingButtons = false);
+      }).
+      catch(() => { /*Do nothing.*/ });
   }
-}
+});
 
 function run() {
   const pageContent = document.querySelector('[class*="PageContent"]');
 
   if (pageContent) {
     // Set up mutation observer to deal with SPA navigation
-    new MutationObserver(function () {
-      handleUiChange();
-    }).observe(pageContent, {childList: true, subtree: true});
+    new MutationObserver(handleUiChange).
+      observe(pageContent, {childList: true, subtree: true});
   }
 
   // Call it once no matter if we were able to set up mutation observer or not
@@ -116,7 +121,7 @@ function run() {
 
 // Wait for the UI to load completely
 document.addEventListener('readystatechange', function onReadyStateChange() {
-  if (document.readyState === "complete") {
+  if (document.readyState === 'complete') {
     run();
   }
 }, false);
