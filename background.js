f47c4912954a5d0437590cacc3cc6104d6a89acc
Maxim Mig <maxim.mig@jetbrains.com>
2019-08-13 16:05:48 +0300

    Show popup action in cross-platform way

--- background.js
+++ background.js
@@ -1,30 +1,5 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'github.com', schemes: ['https']}
-          })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      },
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'gitlab.com', schemes: ['https']}
-          })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      },
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'bitbucket.org', schemes: ['https']}
-          })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }
-    ]);
-  });
+chrome.runtime.onMessage.addListener((message, sender) => {
+  if (message.type === 'enable-page-action') {
+    chrome.pageAction.show(sender.tab.id);
+  }
 });
