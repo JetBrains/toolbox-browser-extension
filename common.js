3479a8578a65cdcaea5f97342c011b657bbb1f24
Maxim Mig <maxim.mig@jetbrains.com>
2019-08-14 19:33:50 +0300

    TBX-1441: moved enabling of page action from common.js to a dedicated file

--- common.js
+++ common.js
@@ -81,7 +81,3 @@ export const DEFAULT_LANGUAGE_SET = {[DEFAULT_LANGUAGE]: HUNDRED_PERCENT};
 export function getToolboxURN(tool, cloneUrl) {
   return `jetbrains://${tool}/checkout/git?checkout.repo=${cloneUrl}&idea.required.plugins.id=Git4Idea`;
 }
-
-(function enablePageActionIIFE() {
-  chrome.runtime.sendMessage({type: 'enable-page-action'});
-}());
