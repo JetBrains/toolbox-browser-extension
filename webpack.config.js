8610741e7a78d94b980c27d728f90ebcf7134c90
Maxim Mig <maxim.mig@jetbrains.com>
2020-06-01 23:01:30 +0200

    TBX-4041: Build beautified code for production

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 
 const path = require('path');
 
+const TerserPlugin = require('terser-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const LicenseChecker = require('@jetbrains/ring-ui-license-checker');
 
@@ -44,20 +45,50 @@ module.exports = {
     ]
   },
   optimization: {
-    splitChunks: {
-      name: 'common',
-      minChunks: 2
-    }
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        extractComments: false,
+        terserOptions: {
+          extractComments: false,
+          mangle: false,
+          compress: {
+            defaults: false,
+            unused: true,
+            arguments: true,
+            booleans: false,
+            expression: false,
+            sequences: false,
+            /* eslint-disable camelcase */
+            dead_code: true,
+            join_vars: false,
+            keep_classnames: true,
+            keep_fnames: true
+            /* eslint-enable camelcase */
+          },
+          output: {
+            beautify: true,
+            comments: false,
+            /* eslint-disable camelcase */
+            indent_level: 2
+            /* eslint-enable camelcase */
+          }
+        }
+      })
+    ]
   },
   plugins: [
-    new CopyWebpackPlugin({patterns: [
-      {from: 'manifest.json'},
-      {from: 'icons/icon-128.png', to: 'icon-128.png'}, // Replace with logo from package after it's generation
-      {from: 'icons/icon-disabled-128.png', to: 'icon-disabled-128.png'},
-      {from: 'popup/common.css', to: 'jetbrains-toolbox-common.css'},
-      {from: 'popup/clone.html', to: 'jetbrains-toolbox-clone-popup.html'},
-      {from: 'popup/disabled.html', to: 'jetbrains-toolbox-disabled-popup.html'}
-    ]}),
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: 'manifest.json'},
+        {from: 'icons/icon-128.png', to: 'icon-128.png'}, // Replace with logo from package after it's generation
+        {from: 'icons/icon-disabled-128.png', to: 'icon-disabled-128.png'},
+        {from: 'popup/common.css', to: 'jetbrains-toolbox-common.css'},
+        {from: 'popup/clone.html', to: 'jetbrains-toolbox-clone-popup.html'},
+        {from: 'popup/disabled.html', to: 'jetbrains-toolbox-disabled-popup.html'},
+        {from: 'distribution/README.md', to: 'README.md'}
+      ]
+    }),
     new LicenseChecker({
       format: params => params.modules.map(mod => `${mod.name}@${mod.version} (${mod.url})
 ${mod.license.name} (${mod.license.url})`).join('\n\n'),
@@ -66,3 +97,4 @@ ${mod.license.name} (${mod.license.url})`).join('\n\n'),
     })
   ]
 };
+
