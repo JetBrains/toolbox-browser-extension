de763e39ae037827daf6fb082909dc3295199668
Maxim Mig <maxim.mig@jetbrains.com>
2020-06-16 12:06:41 +0200

    TBX-4041: Added built on and build with info to target readme

--- distribution/generate-readme.js
+++ distribution/generate-readme.js
@@ -1,23 +1,36 @@
-const chidProcess = require('child_process');
+const childProcess = require('child_process');
 const fs = require('fs');
+const os = require('os');
 
 const readme = fs.createWriteStream('dist/README.md');
 
 readme.write('This is an open source project which is hosted on GitHub: [https://github.com/JetBrains/toolbox-browser-extension/](https://github.com/JetBrains/toolbox-browser-extension/).\n');
 readme.write('\nUse the following data as your reference:\n');
 
-const branch = chidProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+readme.write('\nBuilt from:\n');
+const branch = childProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 readme.write(`\n* Branch: ${branch}\n`);
-const longSHA = chidProcess.execSync("git rev-parse HEAD").toString().trim();
+const longSHA = childProcess.execSync("git rev-parse HEAD").toString().trim();
 readme.write(`* Commit SHA: ${longSHA}\n`);
-const shortSHA = chidProcess.execSync("git rev-parse --short HEAD").toString().trim();
+const shortSHA = childProcess.execSync("git rev-parse --short HEAD").toString().trim();
 readme.write(`* Commit short SHA: ${shortSHA}\n`);
-const authorName = chidProcess.execSync("git log -1 --pretty=format:'%an'").toString().trim();
+const authorName = childProcess.execSync("git log -1 --pretty=format:'%an'").toString().trim();
 readme.write(`* Commit author: ${authorName}\n`);
-const commitTime = chidProcess.execSync("git log -1 --pretty=format:'%cd'").toString().trim();
+const commitTime = childProcess.execSync("git log -1 --pretty=format:'%cd'").toString().trim();
 readme.write(`* Commit time: ${commitTime}\n`);
-const commitMsg = chidProcess.execSync("git log -1 --pretty=%B").toString().trim();
+const commitMsg = childProcess.execSync("git log -1 --pretty=%B").toString().trim();
 readme.write(`* Commit message: ${commitMsg}\n`);
+
+readme.write('\nBuilt on:\n');
+readme.write(`* Platform: ${os.platform()}\n`);
+readme.write(`* Type: ${os.type()}\n`);
+readme.write(`* Release: ${os.release()}\n`);
+
+readme.write('\nBuilt with:\n');
+const nodeVersion = process.version.substring(1);
+readme.write(`* Node: ${nodeVersion}\n`);
+const yarnVersion = childProcess.execSync("yarn -v").toString().trim();
+readme.write(`* Yarn: ${yarnVersion}\n`);
 
 readme.write('\nHow to build:\n');
 readme.write('\n1. \`git clone git@github.com:JetBrains/toolbox-browser-extension.git\`\n');
