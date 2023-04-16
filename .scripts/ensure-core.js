3e2b8f45785ce92a440b002dc80f76981c59688e
Maxim Mig <maxim.mig@jetbrains.com>
2023-04-16 21:53:31 +0200

    Added script that checks if `core-js` versions in `package.json` and `babel.config.json` match

---
+++ .scripts/ensure-core.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const readJsonFile = path => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (ioError, fileContent) => {
+    if (ioError) {
+      reject(ioError);
+    } else {
+      try {
+        const parsedContent = JSON.parse(fileContent);
+        resolve(parsedContent);
+      } catch (e) {
+        reject(e);
+      }
+    }
+  });
+});
+
+Promise
+  .all([readJsonFile('./package.json'), readJsonFile('./babel.config.json')])
+  .then(([packageJson, babelConfigJson]) => {
+    const installedCoreJsVersion = packageJson.devDependencies['core-js'].replace('^', '');
+    const requiredCoreJsVersion = babelConfigJson.presets[0][1].corejs;
+
+    if (!installedCoreJsVersion.startsWith(requiredCoreJsVersion)) {
+      throw new Error(`The current version of core-js is ${installedCoreJsVersion}, but the babel config requires version ${requiredCoreJsVersion}.x`);
+    }
+  })
+  .catch(e => {
+    console.error(e.message);
+  });
