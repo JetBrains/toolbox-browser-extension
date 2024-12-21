76de2750193b62fa474102603fd0c4f61fa44437
Maxim Mig <maxim.mig@jetbrains.com>
2024-12-21 20:15:46 +0100

    Cleanup toolbox.js

--- api/toolbox.js
+++ api/toolbox.js
@@ -5,25 +5,19 @@ export const parseLineNumber = (lineNumber) => {
   return isNaN(parsedValue) ? 1 : parsedValue;
 };
 
-export function getToolboxURN(toolTag, cloneUrl) {
-  return `jetbrains://${toolTag}/checkout/git?checkout.repo=${cloneUrl}&idea.required.plugins.id=Git4Idea`;
-}
+export const getToolboxURN = (toolTag, cloneUrl) =>
+  `jetbrains://${toolTag}/checkout/git?checkout.repo=${cloneUrl}&idea.required.plugins.id=Git4Idea`;
 
-export function getToolboxNavURN(
-  toolTag,
-  project,
-  filePath,
-  lineNumber = null,
-) {
+export const getToolboxNavURN = (toolTag, project, filePath, lineNumber = null) => {
   const lineIndex = convertNumberToIndex(lineNumber == null ? 1 : lineNumber);
   const columnIndex = convertNumberToIndex(1);
   const encodedToolTag = encodeURIComponent(toolTag);
   const encodedProject = encodeURIComponent(project);
 
   return `jetbrains://${encodedToolTag}/navigate/reference?project=${encodedProject}&path=${filePath}:${lineIndex}:${columnIndex}`;
-}
+};
 
-export function callToolbox(action) {
+export const callToolbox = (action) => {
   const fakeAction = document.createElement("a");
   fakeAction.style.position = "absolute";
   fakeAction.style.left = "-9999em";
@@ -31,4 +25,4 @@ export function callToolbox(action) {
   document.body.appendChild(fakeAction);
   fakeAction.click();
   document.body.removeChild(fakeAction);
-}
+};
