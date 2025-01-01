e8eaad8117bb896b024bcaf3fc828362bd2f0c13
Maxim Mig <maxim.mig@jetbrains.com>
2025-01-01 22:14:11 +0100

    Fix script

--- .scripts/prepare-metadata.sh
+++ .scripts/prepare-metadata.sh
@@ -4,4 +4,4 @@ set -e -x -u
 
 printenv CHROME_EXTENSION_PEM_BASE64 | tr -d '\r' | base64 -d > extension.pem
 
-sed -i "s/\"version\": \"1.0\"/\"version\": \"$BUILD_NUMBER\"/g" "manifests/$BROWSER/manifest.json"
+sed -i "s/\"version\": \"1.0\"/\"version\": \"$BUILD_NUMBER\"/g" "src/manifests/$BROWSER/manifest.json"
